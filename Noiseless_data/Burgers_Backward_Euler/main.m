65416348bc575f445b10358609161a8fb38c7073
maziarraissi <maziar.raissi@gmail.com>
2017-03-09 00:55:51 -0500

    Figure 2

--- Noiseless_data/Burgers_Backward_Euler/main.m
+++ Noiseless_data/Burgers_Backward_Euler/main.m
@@ -1,9 +1,10 @@
-clear all; clc; close all;
+function main()
+clc; close all;
 
 addpath ./Utilities
 addpath ./Exact
-addpath ./Kernels_NN
-addpath ~/export_fig
+addpath ./Kernels
+addpath ./export_fig
 
 rng('default')
 
@@ -50,7 +51,6 @@ xstar = linspace(-1,1,400)';
 if plt == 1
     fig = figure(1);
     set(fig,'units','normalized','outerposition',[0 0 1 .5])
-    %set(fig,'units','normalized','outerposition',[0 0 1 1])
     clf
     color2 = [217,95,2]/255;
     k = 1;
@@ -58,12 +58,10 @@ if plt == 1
     hold
     plot(xstar,InitialCondition(xstar),'b','LineWidth',3);
     plot(ModelInfo.x_u, ModelInfo.u,'ro','MarkerSize',12,'LineWidth',3);
-    %yLimits = get(gca,'YLim');
     xlabel('$-1 \leq x \leq 1$')
     ylabel('$u(0,x)$')
     axis square
     ylim([-1.5 1.5]);
-    %ylim([yLimits(1) yLimits(2)]);
     set(gca, 'XTick', sort(ModelInfo.x_u));
     set(gca, 'XTickLabel', [])
     set(gca,'TickLength',[0.05 0.05]);
@@ -72,7 +70,6 @@ if plt == 1
     tit = sprintf('Time: %.2f\n%d training points', 0,Ntr);
     title(tit);
     
-    % w = waitforbuttonpress;
     drawnow;
 end
 
@@ -91,7 +88,6 @@ for i = 1:nsteps
     fprintf(1,'Step: %d, Time = %f, NLML = %e, error_u = %e\n', i, i*dt, NLML, error(i));
     
     x_u = bsxfun(@plus,lb,bsxfun(@times,   lhsdesign(Ntr_artificial,dim)    ,(ub-lb)));
-    %x_u = x_u(x_u>.025 | x_u<-0.025);
     [ModelInfo.u, ModelInfo.S0] = predictor(x_u);
     ModelInfo.x_u = x_u;
     if plt == 1 && mod(i,floor(nsteps/(2*num_plots-1)))==0
@@ -102,13 +98,10 @@ for i = 1:nsteps
         plot(xstar, Kpred,'r--','LineWidth',3);
         [l,p] = boundedline(xstar, Kpred, 2.0*sqrt(Kvar), ':', 'alpha','cmap', color2);
         outlinebounds(l,p);
-        %plot(ModelInfo.x0, ModelInfo.y0,'o','MarkerSize',10,'LineWidth',3);
-        %yLimits = get(gca,'YLim');
         xlabel('$-1 \leq x \leq 1$')
         ylabel('$u(t,x)$')
         axis square
         ylim([-1.5 1.5]);
-        %ylim([yLimits(1) yLimits(2)]);
         set(gca, 'XTick', sort(ModelInfo.x_u));
         set(gca, 'XTickLabel', [])
         set(gca,'TickLength',[0.05 0.05]);
@@ -117,8 +110,6 @@ for i = 1:nsteps
         tit = sprintf('Time: %.2f\nTime: %e\n%d artificial data', i*dt,error(i),Ntr_artificial);
         title(tit);
         
-        
-        % w = waitforbuttonpress;
         drawnow;
         
     end
@@ -127,3 +118,8 @@ for i = 1:nsteps
 end
 
 export_fig ./Figures/Burgers_noiseless.png -r300
+
+rmpath ./Utilities
+rmpath ./Exact
+rmpath ./Kernels
+rmpath ./export_fig
