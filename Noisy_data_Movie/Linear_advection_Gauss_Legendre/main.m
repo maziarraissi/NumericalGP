e7d91a0017bac1ac3568b78bb5f4c91bcb44cbde
maziarraissi <maziar.raissi@gmail.com>
2017-03-09 11:02:06 -0500

    Figure 11

--- Noisy_data_Movie/Linear_advection_Gauss_Legendre/main.m
+++ Noisy_data_Movie/Linear_advection_Gauss_Legendre/main.m
@@ -1,8 +1,8 @@
-clear all; clc; close all
+function main()
+clc; close all
 
 addpath ./Kernels
 addpath ./Utilities
-addpath ~/export_fig
 
 rng('default')
 
@@ -30,9 +30,7 @@ nsteps = T/dt;
 nn = 400;
 xstar = linspace(lb(1),ub(1),nn)';
 
-num_plots = 3;
-
-movObj = QTWriter('Advection_noisy.mov');
+movObj = QTWriter('./Movies/Advection_noisy.mov');
 movObj.FrameRate = 4;
 
 %% Optimize model
@@ -50,21 +48,16 @@ ModelInfo.hyp = log([1 1 1 1 1 1 10^-6]);
 
 if plt == 1
     fig = figure(1);
-%     set(fig,'units','normalized','outerposition',[0 0 1 .5])
     set(fig,'units','normalized','outerposition',[0 0 1 1])
     clf
     color2 = [217,95,2]/255;
-%     k = 1;
-%     subplot(2,num_plots,k)
     hold
     plot(xstar,Exact_solution(0,xstar),'b','LineWidth',3);
     plot(ModelInfo.x_u, ModelInfo.u,'ro','MarkerSize',12,'LineWidth',3);
-    %yLimits = get(gca,'YLim');
     xlabel('$0 \leq x \leq 1$')
     ylabel('$u(0,x)$')
     axis square
     ylim([-1.5 1.5]);
-    %ylim([yLimits(1) yLimits(2)]);
     set(gca, 'XTick', sort(ModelInfo.x_u));
     set(gca, 'XTickLabel', [])
     set(gca,'TickLength',[0.05 0.05]);
@@ -73,8 +66,6 @@ if plt == 1
     tit = sprintf('Time: %.2f\n%d training points', 0,Ntr);
     title(tit);
     
-    
-    % w = waitforbuttonpress;
     drawnow;
     
     writeMovie(movObj, getframe(fig));
@@ -85,7 +76,6 @@ for i = 1:nsteps
     
     [ModelInfo.hyp,~,~] = minimize(ModelInfo.hyp, @likelihood, -5000);
     [NLML,~]=likelihood(ModelInfo.hyp);
-    % exp(ModelInfo.hyp)
     
     [Kpred, Kvar] = predictor(xstar);
     Kvar = abs(diag(Kvar));
@@ -99,30 +89,17 @@ for i = 1:nsteps
     ModelInfo.x_u = x_u;
     
     
-%     for j = 2:nn-1
-%         u_Lax(i+1,j) = aa*u_Lax(i,j-1) + bb*u_Lax(i,j) + cc*u_Lax(i,j+1);
-%     end
-%     u_Lax(i+1,1) = aa*u_Lax(i,nn-1) + bb*u_Lax(i,1) + cc*u_Lax(i,2);
-%     u_Lax(i+1,end) = aa*u_Lax(i,nn-1) + bb*u_Lax(i,end) + cc*u_Lax(i,2);
-    
-    if plt == 1 %&& mod(i,floor(nsteps/(2*num_plots-1)))==0
-%         k = k+1;
-%         subplot(2,num_plots,k)
+    if plt == 1
         clf
         hold
         plot(xstar,Exact,'b','LineWidth',3);
         plot(xstar, Kpred,'r--','LineWidth',3);
-%         plot(xstar, u_Lax(i+1,:), 'k:','LineWidth',1);
-        % plot(ModelInfo.x0, ModelInfo.y0,'ro','MarkerSize',12,'LineWidth',1);
         [l,p] = boundedline(xstar, Kpred, 2.0*sqrt(Kvar), ':', 'alpha','cmap', color2);
         outlinebounds(l,p);
-        %plot(ModelInfo.x0, ModelInfo.y0,'o','MarkerSize',10,'LineWidth',3);
-        %yLimits = get(gca,'YLim');
         xlabel('$0 \leq x \leq 1$')
         ylabel('$u(t,x)$')
         axis square
         ylim([-1.5 1.5]);
-        %ylim([yLimits(1) yLimits(2)]);
         set(gca, 'XTick', sort(ModelInfo.x_u));
         set(gca, 'XTickLabel', [])
         set(gca,'TickLength',[0.05 0.05]);
@@ -131,7 +108,6 @@ for i = 1:nsteps
         tit = sprintf('Time: %.2f\n%d artificial data', i*dt ,Ntr_artificial);
         title(tit);
         
-        % w = waitforbuttonpress;
         drawnow;
         
         writeMovie(movObj, getframe(fig));
@@ -144,13 +120,5 @@ end
 movObj.Loop = 'backandforth';
 close(movObj);
 
-
-% buf = sprintf('error_dt%.0e_noise_%f.txt', dt, noise);
-% save(buf,'-ascii','-double', 'error');
-% figure(2)
-% clf
-% hold
-% plot((dt:dt:T), error)
-%
-%
-export_fig ./Figures/Advection.png -r300
+rmpath ./Kernels
+rmpath ./Utilities
